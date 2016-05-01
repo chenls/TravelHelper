429ae7a7ccb809160e5ef063ef1c7b108b6e55a2
chenls <923847753@qq.com>
2016-05-01 15:34:54 +0000

    添加设置功能

--- app/src/main/java/com/cqupt/travelhelper/activity/MainActivity.java
+++ app/src/main/java/com/cqupt/travelhelper/activity/MainActivity.java
@@ -2,22 +2,34 @@ package com.cqupt.travelhelper.activity;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.TabLayout;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.cqupt.travelhelper.R;
 import com.cqupt.travelhelper.fragment.AttractionFragment;
 import com.cqupt.travelhelper.fragment.MineFragment;
 import com.cqupt.travelhelper.fragment.StrategyFragment;
 import com.cqupt.travelhelper.fragment.TravelsFragment;
+import com.cqupt.travelhelper.utils.CommonUtil;
+import com.pgyersdk.feedback.PgyFeedbackShakeManager;
+import com.pgyersdk.javabean.AppBean;
+import com.pgyersdk.update.PgyUpdateManager;
+import com.pgyersdk.update.UpdateManagerListener;
+import com.pgyersdk.views.PgyerDialog;
 
 public class MainActivity extends AppCompatActivity {
     TabLayout tabLayout;
@@ -29,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkUpdate();
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         viewPager = (ViewPager) findViewById(R.id.viewPager);
         toolBar = (Toolbar) findViewById(R.id.toolBar);
@@ -113,5 +126,97 @@ public class MainActivity extends AppCompatActivity {
                 mineFragment.changeInformation(bitmap, name);
             }
         }
+    }
+
+    private void checkUpdate() {
+        //蒲公英自动更新
+        PgyUpdateManager.register(MainActivity.this,
+                new UpdateManagerListener() {
+                    @Override
+                    public void onUpdateAvailable(final String result) {
+                        // 将新版本信息封装到AppBean中
+                        final AppBean appBean = getAppBeanFromString(result);
+                        //如果WIFI可用且用户设置了自动下载  不弹对话框，直接下载
+                        if (CommonUtil.isWifiAvailable(MainActivity.this)) {
+                            SharedPreferences prefs = PreferenceManager
+                                    .getDefaultSharedPreferences(MainActivity.this);
+                            if (prefs.getBoolean("update", true)) {
+                                Toast.makeText(MainActivity.this, "检测到WIFI可用，" +
+                                        "\n开始自动下载更新安装包", Toast.LENGTH_LONG).show();
+                                startDownloadTask(MainActivity.this, appBean.getDownloadURL());
+                            }
+                            return;
+                        }
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("更新应用")
+                                .setMessage(appBean.getReleaseNote())
+                                .setPositiveButton(
+                                        "确定"
+                                        , new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                startDownloadTask(MainActivity.this,
+                                                        appBean.getDownloadURL());
+                                            }
+                                        })
+                                .setNegativeButton(
+                                        "取消",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                            }
+                                        }).show();
+                    }
+
+                    @Override
+                    public void onNoUpdateAvailable() {
+                    }
+                });
+    }
+
+    //蒲公英 反馈相关
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getBoolean("shake", true)) {
+            // 摇一摇的灵敏度，默认为950，数值越小灵敏度越高。
+            PgyFeedbackShakeManager.setShakingThreshold(800);
+            // 以对话框的形式弹出
+            PgyFeedbackShakeManager.register(MainActivity.this);
+            //noinspection ResourceType
+            PgyerDialog.setDialogTitleBackgroundColor(getString(R.color.colorPrimary));
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        PgyFeedbackShakeManager.unregister();
+    }
+
+    private long exitTime = 0;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK
+                && event.getAction() == KeyEvent.ACTION_DOWN) {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+            if (prefs.getBoolean("back", true)) {
+                if ((System.currentTimeMillis() - exitTime) > 2000) {
+                    Toast.makeText(MainActivity.this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+                    exitTime = System.currentTimeMillis();
+                } else {
+                    Intent intent = new Intent(Intent.ACTION_MAIN);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.addCategory(Intent.CATEGORY_HOME);
+                    startActivity(intent);
+                    finish();
+                    System.exit(0);
+                }
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
--- app/src/main/java/com/cqupt/travelhelper/activity/SettingsActivity.java
+++ app/src/main/java/com/cqupt/travelhelper/activity/SettingsActivity.java
@@ -1,15 +1,71 @@
 package com.cqupt.travelhelper.activity;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
 
 import com.cqupt.travelhelper.R;
+import com.cqupt.travelhelper.switchButton.SwitchButton;
 
-public class SettingsActivity extends AppCompatActivity {
+import cn.bmob.push.BmobPush;
 
+public class SettingsActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SwitchButton update_switch_button = (SwitchButton) findViewById(R.id.update_switch_button);
+        assert update_switch_button != null;
+        update_switch_button.setOnCheckedChangeListener(this);
+        update_switch_button.setCheckedDelayed(prefs.getBoolean("update", true));
+        SwitchButton push_switch_button = (SwitchButton) findViewById(R.id.push_switch_button);
+        assert push_switch_button != null;
+        push_switch_button.setOnCheckedChangeListener(this);
+        push_switch_button.setCheckedDelayed(prefs.getBoolean("push", true));
+        SwitchButton shake_switch_button = (SwitchButton) findViewById(R.id.shake_switch_button);
+        assert shake_switch_button != null;
+        shake_switch_button.setOnCheckedChangeListener(this);
+        shake_switch_button.setCheckedDelayed(prefs.getBoolean("shake", true));
+        SwitchButton back_switch_button = (SwitchButton) findViewById(R.id.back_switch_button);
+        assert back_switch_button != null;
+        back_switch_button.setOnCheckedChangeListener(this);
+        back_switch_button.setCheckedDelayed(prefs.getBoolean("back", true));
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId()) {
+            case R.id.update_switch_button:
+                saveData("update", isChecked);
+                break;
+            case R.id.push_switch_button:
+                saveData("push", isChecked);
+                if (isChecked) {
+                    // 启动推送服务
+                    BmobPush.startWork(this);
+                } else {
+                    // 关闭推送服务
+                    BmobPush.stopWork();
+                }
+                break;
+            case R.id.shake_switch_button:
+                saveData("shake", isChecked);
+                break;
+            case R.id.back_switch_button:
+                saveData("back", isChecked);
+                break;
+        }
+    }
+
+    private void saveData(String key, boolean value) {
+        SharedPreferences sharedPreferences = getSharedPreferences(getPackageName() + "_preferences"
+                , Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(key, value);
+        editor.apply();
     }
 }
